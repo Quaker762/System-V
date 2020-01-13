0528a25fcd8cb0bb61cc729bf0636076c37eeb7c
Jesse Buhagiar <jesse.buhagiar@student.rmit.edu.au>
2020-01-13 23:12:19 +1100

    Kernel: Halt function and `InterruptDisabler`
    Just a first implementation of some handy dandy CPU stuff that
    will make life easier further on down the road. The `InterruptDisabler`
    class is a neat little trick I learned from Andreas Kling working
    on SerenityOS which uses the principle of object lifetime to
    automagically enable and disable interrupts for the lifetime
    of a function

---
+++ kernel/arch/arm/cpu.h
@@ -0,0 +1,33 @@
+/**
+ * CPU related classes for the Cortex-A15 Processor
+ */
+#pragma once
+
+#include <stdint.h>
+
+[[noreturn]] static inline void halt()
+{
+    __asm__ volatile("cpsid iaf");  // Disable all interrupts (IRQ, FIQ and **)
+    __asm__ volatile("wfi");        // Wait for an interrupt (which can never happen!)
+}
+
+// A clever trick from Serenity OS. 
+// This class is instantiated at the beginning of a function
+// that should have interrupts disabled. The constructor emits the
+// instruction "cpsid iaf", and the destructor emits "cpsie iaf", meaning
+// that when the function leaves scope (and hence the object is destroyed)
+// interrupts are automagically re-enabled without us having to
+// remember to do it ourselves!
+class InterruptDisabler final
+{
+public:
+    inline InterruptDisabler()
+    {
+        __asm__ volatile("cpsid iaf");
+    }
+
+    inline ~InterruptDisabler()
+    {
+        __asm__ volatile("cpsie iaf");
+    }
+};
