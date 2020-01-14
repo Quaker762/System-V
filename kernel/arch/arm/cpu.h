94630b406052df1a00997e0cd20eb1c861e8a64c
mjatkin <matthewjamesatkin@gmail.com>
2020-01-14 23:15:58 +1100

    Kernel: Create CPUID class
    This class extracts the processor information from the Main ID Register and extracts each component into its own variable. This variables might still need to be decoded into something more readable.

--- kernel/arch/arm/cpu.h
+++ kernel/arch/arm/cpu.h
@@ -32,3 +32,29 @@ public:
         __asm__ volatile("cpsie iaf");
     }
 };
+
+class CPUID
+{
+public:
+    inline CPUID()
+    {
+        __asm__ volatile("mrc p15, 0, %[result], c0, c0, 0" : [result] "=r" (m_MID));
+
+        m_implementer =         (m_MID >> 24);
+        m_variant =             (m_MID >> 20) & 0xF;
+        m_arch =                (m_MID >> 16) & 0xF;
+        m_primary_part_number = (m_MID >> 4)  & 0xFFF; 
+        m_rev =                  m_MID        & 0xF;
+    }
+
+    // TODO: create a decoder for the information extracted
+
+    uint32_t m_MID;                   // 32-bits
+
+    uint8_t  m_implementer;           // 8-bits
+    uint8_t  m_variant;               // 4-bits
+    uint8_t  m_arch;                  // 4-bits
+    uint16_t m_primary_part_number;   // 12-bits
+    uint8_t  m_rev;                   // 4-bits
+
+};
