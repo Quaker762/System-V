cc1767fbb4a890a53f6ecf1673563329ad53de67
mjatkin <matthewjamesatkin@gmail.com>
2020-01-18 01:07:00 +1100

    mjlib: Start mjlib
    Added simple string class to populate it, as well as the makefile

---
+++ mjlib/string.cpp
@@ -0,0 +1,18 @@
+#include "string.h"
+
+namespace MJ {
+
+    String::String(const char* cstring) {
+        char character = 0;
+        for(int i = 0; character != '\0'; ++i) {
+            character = *(cstring + i);
+            m_characters[i] = character;
+        }
+    }
+
+    String::String(const char* cstring, size_t length) {
+        for(int i = 0; i < static_cast<int>(length); ++i) {
+            m_characters[i] = *(cstring + i);
+        }
+    }
+}
---
+++ mjlib/string.h
@@ -0,0 +1,18 @@
+#include <stddef.h>
+
+namespace MJ {
+
+class String {
+public:
+    ~String() {}
+
+    String() {}
+
+    String(const char* cstring);
+
+    String(const char* cstring, size_t length);
+
+private:
+    char m_characters[256];
+};
+}
