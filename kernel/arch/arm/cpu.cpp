79e55990be06b42b8e39fbaea8a57c5ded489c21
Jesse Buhagiar <jesse.buhagiar@student.rmit.edu.au>
2020-01-20 01:05:35 +1100

    Kernel: Log why assertion failed
    Assertion failures are now printed to UART0 using out phresh
    and new kprintf.

--- kernel/arch/arm/cpu.cpp
+++ kernel/arch/arm/cpu.cpp
@@ -1,11 +1,13 @@
 /**
  *
  */
+#include <kernel/kstdlib/kstdio.h>
 #include "arch/arm/cpu.h"
 #include "assertions.h"
 
-void __assertion_failed(const char*, const char*, unsigned, const char*)
+void __assertion_failed(const char* msg, const char* file, unsigned line, const char* func)
 {
+    kprintf("\nASSERTION FAILED %s\n%s:%d (%s)\n", msg, file, line, func);
     for(;;)
         ;
 }
