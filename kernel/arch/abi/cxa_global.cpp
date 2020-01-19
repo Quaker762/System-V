0ef842dd5d09b35e5d78d3fcf4ebd85a794464b3
Jesse Buhagiar <jesse.buhagiar@student.rmit.edu.au>
2020-01-19 21:02:14 +1100

    Kernel: Initialise global constructors on start
    Any object that's global (outside of a function) now has it's
    constructors initialised before we make the call to `init()`.

---
+++ kernel/arch/abi/cxa_global.cpp
@@ -0,0 +1,15 @@
+/**
+ * Initialises all global constructors by calling function pointers that are located
+ * in the .init_array* sections in the ELF binary
+ */
+extern "C"
+{
+    typedef void (*ctor)(void);
+    extern ctor __init_array_start[0], __init_array_end[0];
+
+    void __cxa_global_constructors(void)
+    {
+        for(ctor* func = __init_array_start; func != __init_array_end; func++)
+            (*func)();
+    }
+}
--- kernel/arch/arm/start.s
+++ kernel/arch/arm/start.s
@@ -1,10 +1,12 @@
 .section .text.boot
 
 .extern init
+.extern __cxa_global_constructors
 
 .global _start
 _start:
     ldr sp, =__STACK_TOP
+    bl __cxa_global_constructors
     b init
 
 # If we end up here, the kernel's returned!! (not good!)
