68a0d1611509619730cc015aaed7546f618223c6
Jesse Buhagiar <jesse.buhagiar@student.rmit.edu.au>
2020-01-20 23:58:50 +1100

    Kernel: Reorder register_dump and implement spsr helper
    The way in which we were reading the registers from the stack
    was in the wrong order. This fixes that so we can now actually
    access the value of spsr.
    
    Also implement a nice helper class to decode the bit patterns
    found in cpsr/spsr.

--- kernel/arch/arm/cpu.h
+++ kernel/arch/arm/cpu.h
@@ -7,18 +7,19 @@
 
 [[noreturn]] static inline void halt()
 {
-    __asm__ volatile("cpsid iaf");  // Disable all interrupts (IRQ, FIQ and **)
-    __asm__ volatile("wfi");        // Wait for an interrupt (which can never happen!)
-    for(;;);
+    __asm__ volatile("cpsid iaf"); // Disable all interrupts (IRQ, FIQ and **)
+    __asm__ volatile("wfi");       // Wait for an interrupt (which can never happen!)
+    for(;;)
+        ;
 }
 
 struct register_dump
 {
-    uint32_t r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, fp, r12, lr, pc;
     uint32_t spsr;
-};
+    uint32_t r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, fp, r12, lr, pc;
+} __attribute__((packed));
 
-// A clever trick from Serenity OS. 
+// A clever trick from Serenity OS.
 // This class is instantiated at the beginning of a function
 // that should have interrupts disabled. The constructor emits the
 // instruction "cpsid iaf", and the destructor emits "cpsie iaf", meaning
@@ -39,7 +40,43 @@ public:
     }
 };
 
-extern "C" void relocate_vector_table();
+enum class ProcessorMode : uint8_t
+{
+    USER = 0b0000,
+    FIQ = 0b0001,
+    IRQ = 0b0010,
+    KERNEL_MODE = 0b0011,
+    MONITOR = 0b0110,
+    ABORT = 0b0111,
+    HYPERVISOR = 0b1010,
+    UNDEFINED = 0b1011,
+    SYSTEM = 0b1111
+};
+
+class StatusRegister
+{
+public:
+    StatusRegister()
+    {
+        __asm__ volatile("mrs %[result], cpsr"
+                         : [result] "=r"(m_cpsr));
+        __asm__ volatile("mrs %[result], spsr"
+                         : [result] "=r"(m_spsr));
+    }
+
+    uint32_t cpsr() const { return m_cpsr; }
+    uint32_t spsr() const { return m_spsr; }
+
+    ProcessorMode get_current_mode() const { return static_cast<ProcessorMode>(m_cpsr & 0x03); }
+    ProcessorMode get_pre_exception_mode() const { return static_cast<ProcessorMode>(m_spsr & 0x03); }
+
+private:
+    uint32_t m_cpsr { 0 };
+    uint32_t m_spsr { 0 };
+};
+
+extern "C" void
+relocate_vector_table();
 extern "C" void* vector_table;
 
 class CPUID
@@ -47,23 +84,23 @@ class CPUID
 public:
     inline CPUID()
     {
-        __asm__ volatile("mrc p15, 0, %[result], c0, c0, 0" : [result] "=r" (m_MID));
+        __asm__ volatile("mrc p15, 0, %[result], c0, c0, 0"
+                         : [result] "=r"(m_MID));
 
-        m_implementer =         (m_MID >> 24);
-        m_variant =             (m_MID >> 20) & 0xF;
-        m_arch =                (m_MID >> 16) & 0xF;
-        m_primary_part_number = (m_MID >> 4)  & 0xFFF; 
-        m_rev =                  m_MID        & 0xF;
+        m_implementer = (m_MID >> 24);
+        m_variant = (m_MID >> 20) & 0xF;
+        m_arch = (m_MID >> 16) & 0xF;
+        m_primary_part_number = (m_MID >> 4) & 0xFFF;
+        m_rev = m_MID & 0xF;
     }
 
     // TODO: create a decoder for the information extracted
 
-    uint32_t m_MID;                   // 32-bits
+    uint32_t m_MID; // 32-bits
 
-    uint8_t  m_implementer;           // 8-bits
-    uint8_t  m_variant;               // 4-bits
-    uint8_t  m_arch;                  // 4-bits
-    uint16_t m_primary_part_number;   // 12-bits
-    uint8_t  m_rev;                   // 4-bits
-
+    uint8_t m_implementer;          // 8-bits
+    uint8_t m_variant;              // 4-bits
+    uint8_t m_arch;                 // 4-bits
+    uint16_t m_primary_part_number; // 12-bits
+    uint8_t m_rev;                  // 4-bits
 };
