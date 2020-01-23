484ec563048f6a1e0c45f9444ce9fe6926e7fb5e
Jesse Buhagiar <jesse.buhagiar@student.rmit.edu.au>
2020-01-23 21:17:15 +1100

    Kernel: Dump more registers in `kpanic()`
    `kpanic()` now dumps some more important registers to the
    UART, namely all of the FAULT (*FAR, *FSR) registers.

--- kernel/arch/arm/cpu.cpp
+++ kernel/arch/arm/cpu.cpp
@@ -19,13 +19,29 @@ enum class ExceptionType
 
 [[noreturn]] static void kpanic(const ExceptionType& type, const register_dump& regs)
 {
+    uint32_t ifsr, dfsr;
+    uint32_t ifar, dfar;
+    uint32_t far;
+
+    __asm__ volatile("mrc p15, 0, %0, c5, c0, 1"
+                     : "=r"(ifsr));
+    __asm__ volatile("mrc p15, 0, %0, c5, c0, 0"
+                     : "=r"(dfsr));
+    __asm__ volatile("mrc p15, 0, %0, c6, c0, 2"
+                     : "=r"(ifar));
+    __asm__ volatile("mrc p15, 0, %0, c6, c0, 0"
+                     : "=r"(dfar));
+    __asm__ volatile("mrc p15, 0, %0, c6, c0, 1"
+                     : "=r"(far));
+
     //TODO: We need to check whether or not this occured in Kernel Mode or User Mode
     kprintf("r0:  0x%x r1: 0x%x r2:   0x%x r3: 0x%x\n", regs.r0, regs.r1, regs.r2, regs.r3);
     kprintf("r4:  0x%x r5: 0x%x r6:   0x%x r7: 0x%x\n", regs.r4, regs.r5, regs.r6, regs.r7);
     kprintf("r8:  0x%x r9: 0x%x r10:  0x%x fp: 0x%x\n", regs.r8, regs.r9, regs.r10, regs.fp);
-    kprintf("r12: 0x%x lr: 0x%x spsr: 0x%x\n\n", regs.r12, regs.lr, regs.spsr);
-    kprintf("DFSR: 0x%x IFSR: 0x%x\n", 0, 0);
-    kprintf("DFAR: 0x%x IFAR: 0x%x\n", 0, 0);
+    kprintf("r12: 0x%x lr: 0x%x spsr: 0x%x\n", regs.r12, regs.lr, regs.spsr);
+    kprintf("DFSR: 0x%x IFSR: 0x%x\n", dfsr, ifsr);
+    kprintf("DFAR: 0x%x IFAR: 0x%x\n", dfar, ifar);
+    kprintf("FAR:  0x%x\n", far);
     kprintf("Kernel Panic: ");
     switch(type)
     {
@@ -37,11 +53,8 @@ enum class ExceptionType
         }
         case ExceptionType::DATA_ABORT:
         {
-            // Read the address that caused the fault
-            uint32_t dfar;
-            __asm__ volatile("MRC p15, 0, %0, c6, c0, 0"
-                             : "=r"(dfar));
-            kprintf("Data Abort! Faulting address=0x%x\n", dfar);
+            uint32_t faulting_address = regs.lr - 0x8;
+            kprintf("Data Abort/Page Fault!\nFaulting address @ 0x%x(0x%x)\n", faulting_address, *reinterpret_cast<uint32_t*>(faulting_address));
             break;
         }
         default:
