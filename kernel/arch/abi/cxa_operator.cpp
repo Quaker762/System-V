c8bd4b9266bf8dcf68f61e1ea22dca88b10f3bf5
Jesse Buhagiar <jesse.buhagiar@student.rmit.edu.au>
2020-02-10 19:25:03 +1100

    Kernel: Implement operator new and delete
    These are now nice wrappers for kmalloc :^)

--- kernel/arch/abi/cxa_operator.cpp
+++ kernel/arch/abi/cxa_operator.cpp
@@ -3,12 +3,34 @@
  *   
  */
 
-void operator delete(void*, unsigned int)
+#include <kernel/mem/heap/heap.h>
+
+void* operator new(size_t alloc_size)
 {
-    // Do nothing for now
+    return kmalloc(alloc_size);
 }
 
-void operator delete(void*)
+void* operator new[](size_t alloc_size)
 {
-    // Do nothing for now
+    return kmalloc(alloc_size);
+}
+
+void operator delete(void* ptr)
+{
+    kfree(ptr);
+}
+
+void operator delete(void* ptr, size_t)
+{
+    kfree(ptr);
+}
+
+void operator delete[](void* ptr, size_t)
+{
+    kfree(ptr);
+}
+
+void operator delete[](void* ptr)
+{
+    kfree(ptr);
 }
