df7911b863ff182a40507c0b2aef88945dd59c06
Jesse Buhagiar <jooster669@gmail.com>
2021-06-14 01:10:51 +1000

    Kernel: Do preinit array initialization in cxa_abi

--- kernel/arch/abi/cxa_global.cpp
+++ kernel/arch/abi/cxa_global.cpp
@@ -6,9 +6,13 @@ extern "C"
 {
     typedef void (*ctor)(void);
     extern ctor __init_array_start[0], __init_array_end[0];
+    extern ctor __preinit_array_start[0], __preinit_array_end[0];
 
     void __cxa_global_constructors(void)
     {
+        for(ctor* func = __preinit_array_start; func != __preinit_array_end; func++)
+            (*func)();
+
         for(ctor* func = __init_array_start; func != __init_array_end; func++)
             (*func)();
     }
